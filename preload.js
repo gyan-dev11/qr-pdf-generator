eb16d890ffc3a895d0019284652689ccfbd81985
Gyan Prakash Tiwari <gyan@ratnamsolutions.com>
2025-07-21 10:33:15 +0530

    update

--- preload.js
+++ preload.js
@@ -1,6 +1,27 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  selectFolder: async () => await ipcRenderer.invoke('dialog:selectFolder'),
-  selectFile: async () => await ipcRenderer.invoke('dialog:selectFile'),
+  selectFolder: async () => {
+    try {
+      console.log('[Preload] Requesting folder selection...');
+      const folder = await ipcRenderer.invoke('dialog:selectFolder');
+      console.log('[Preload] Folder selected:', folder);
+      return folder;
+    } catch (err) {
+      console.error('[Preload] Error selecting folder:', err);
+      return null;
+    }
+  },
+
+  selectFile: async () => {
+    try {
+      console.log('[Preload] Requesting file selection...');
+      const file = await ipcRenderer.invoke('dialog:selectFile');
+      console.log('[Preload] File selected:', file);
+      return file;
+    } catch (err) {
+      console.error('[Preload] Error selecting file:', err);
+      return null;
+    }
+  }
 });
